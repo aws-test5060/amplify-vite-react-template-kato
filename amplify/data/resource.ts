0ccf5fcc47ac600e5f74c804e31ffd0a11080856
aws-test5060 <aws-github-test5060@outlook.com>
2025-08-01 18:56:31 +0900

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,30 +1,21 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
+ Todo: a
+   .model({
+     content: a.string(),
+   })
+   .authorization((allow) => [allow.authenticated()]), // 変更
 });
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
-  },
+ schema,
+ authorizationModes: {
+   defaultAuthorizationMode: "userPool", // 変更
+   // API 認証の記述を削除
+ },
 });
 
 /*== STEP 2 ===============================================================
